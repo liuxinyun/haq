b27509ba9d21d0423173cff6f65a3189360dd66e
liuxinyun <liuxinyun@gaosiedu.com>
2017-08-13 12:07:00 +0800

    haq-2.0 修改：spiderTest

--- haq-spider/src/test/java/SpiderTest.java
+++ haq-spider/src/test/java/SpiderTest.java
@@ -23,29 +23,14 @@ public class SpiderTest {
             Document document = Jsoup.connect("http://sports.163.com/17/0812/20/CRLRLGSF00058780.html").timeout(3000).get();
             String content = document.select("div#endText").html();
             Map<String, String> map = DownPicUtil.htmlToFtp(content);
-            System.out.println(map.get("img"));
-            System.out.println(map.get("html"));
-            /*List<String> imageUrl = DownPicUtil.getImageUrl(content);
-            for (String s : imageUrl) {
-                System.out.println(s);
-            }
-            List<String> imageSrc = DownPicUtil.getImageSrc(imageUrl);
-            List<ImgPath> imgPaths = new ArrayList<>();
-            for (String s : imageSrc) {
-                System.out.println(s);
-                String imageName = s.substring(s.lastIndexOf("/") + 1, s.length());
-                System.out.println(imageName);
-                ImgPath imgPath = new ImgPath();
-                imgPath.setSource(s);
-                imgPath.setLocal(imageName);
-                imgPaths.add(imgPath);
-            }
-            String img_path = JSONObject.toJSONString(imgPaths);
+            //System.out.println(map.get("img"));
+            //System.out.println(map.get("html"));
+            String img_path = map.get("img");
             System.out.println(img_path);
-            List<ImgPath> imgPath1 = JSONObject.parseArray(img_path, ImgPath.class);
-            for (ImgPath imgPath : imgPath1) {
+            List<ImgPath> imgPaths = JSONObject.parseArray(img_path, ImgPath.class);
+            for (ImgPath imgPath : imgPaths) {
                 System.out.println(imgPath.toString());
-            }*/
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
