d4a58036b9f6f2672f123f2c01b1fe0278223aa1
liuxinyun <liuxinyun@gaosiedu.com>
2017-08-14 21:24:57 +0800

    图片正则测试

--- haq-spider/src/test/java/SpiderTest.java
+++ haq-spider/src/test/java/SpiderTest.java
@@ -6,7 +6,6 @@ import org.jsoup.nodes.Document;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -34,6 +33,21 @@ public class SpiderTest {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void regex(){
+        String image1 = "<img alt=\"解放军军机5天内3次绕台 台媒:展现不容分裂决心\" src=\"http://cms-bucket.nosdn.127.net/4e9ecef273cd4496bd98ad08a09a27bf20170814091205.jpeg\" style=\"border-width: 0px; border-style: initial; vertical-align: middle; max-width: 640px; display: block; margin: 0px auto;\">";
+        String image2 = "<img style=\"max-width:500px\" src=\"http://n.sinaimg.cn/translate/20170809/ibtW-fyitpmh5638890.jpg\" img-size=\"540,303\">";
+        String html = image1+"<p>ahoaoiiuiu</p>"+image2;
+        List<String> imageUrl = DownPicUtil.getImageUrl(html);
+        for (String s : imageUrl) {
+            System.out.println(s);
+        }
+        List<String> imageSrc = DownPicUtil.getImageSrc(imageUrl);
+        for (String s : imageSrc) {
+            System.out.println(s);
         }
     }
 
